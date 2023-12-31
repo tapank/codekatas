9fb94d3a70dcf640720b0d363bfe0e59418772ce
Tapan Karecha <tapan@karecha.com>
2023-12-31 11:25:27 +0530

    add exercism exercise: bird-watcher

---
+++ exercism/go/bird-watcher/bird_watcher.go
@@ -0,0 +1,30 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	total := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		total += birdsPerDay[i]
+	}
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	total := 0
+	for i := (week - 1) * 7; i < week*7 && i < len(birdsPerDay); i++ {
+		total += birdsPerDay[i]
+	}
+	return total
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i]++
+	}
+	return birdsPerDay
+}
