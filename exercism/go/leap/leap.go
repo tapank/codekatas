cad0551efa2f652bcc6f2e02fdec1584c59da8e1
Tapan Karecha <tapan@karecha.com>
2024-01-09 11:17:42 +0530

    add exercism exercise: leap

---
+++ exercism/go/leap/leap.go
@@ -0,0 +1,12 @@
+package leap
+
+// IsLeapYear returns true if the year is leap
+func IsLeapYear(year int) bool {
+	if year%400 == 0 {
+		return true
+	}
+	if year%100 == 0 {
+		return false
+	}
+	return year%4 == 0
+}
