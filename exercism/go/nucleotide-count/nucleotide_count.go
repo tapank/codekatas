acd7cbc7b89a67c1f66b25f08552a541742ecb24
Tapan Karecha <tapan@karecha.com>
2024-01-22 09:49:30 +0530

    add exercism exercise: nucleotide-count

---
+++ exercism/go/nucleotide-count/nucleotide_count.go
@@ -0,0 +1,26 @@
+package dna
+
+import (
+	"errors"
+)
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int
+
+// DNA is a list of nucleotides.
+type DNA string
+
+// Counts generates a histogram of valid nucleotides in the given DNA.
+// Returns an error if d contains an invalid nucleotide.
+func (d DNA) Counts() (Histogram, error) {
+	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	for _, r := range d {
+		switch r {
+		case 'A', 'C', 'G', 'T':
+			h[r]++
+		default:
+			return nil, errors.New("Unknown nucleotide: " + string(r))
+		}
+	}
+	return h, nil
+}
