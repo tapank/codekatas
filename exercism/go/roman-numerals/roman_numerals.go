2b279da29a16a5f6ac2584c96bf7fbd5e5381f6e
Tapan Karecha <tapan@karecha.com>
2024-01-25 17:14:28 +0530

    add exercism exercise: roman-numerals v2

--- exercism/go/roman-numerals/roman_numerals.go
+++ exercism/go/roman-numerals/roman_numerals.go
@@ -5,23 +5,14 @@ import (
 	"fmt"
 )
 
-var thousands = []string{"", "M", "MM", "MMM"}
-var hundreds = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
-var tens = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
-var units = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+var M = []string{"", "M", "MM", "MMM"}
+var C = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+var X = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+var I = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
 
-func ToRomanNumeral(input int) (string, error) {
-	if input < 1 || input > 3999 {
-		return "", errors.New("Invalid input: " + fmt.Sprint(input))
+func ToRomanNumeral(n int) (string, error) {
+	if n < 1 || n > 3999 {
+		return "", errors.New("Invalid input: " + fmt.Sprint(n))
 	}
-	out := ""
-
-	out += thousands[input/1000]
-	input %= 1000
-	out += hundreds[input/100]
-	input %= 100
-	out += tens[input/10]
-	input %= 10
-	out += units[input]
-	return string(out), nil
+	return M[n/1000] + C[(n%1000)/100] + X[(n%100)/10] + I[n%10], nil
 }
