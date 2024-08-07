b4eb24e6988b8b715076bd20649e1ccf5b92fdc5
Tapan Karecha <tapan@karecha.com>
2024-08-07 22:27:19 +0530

    update exercism go exercise: dominoes

--- exercism/go/dominoes/dominoes.go
+++ exercism/go/dominoes/dominoes.go
@@ -2,32 +2,33 @@ package dominoes
 
 type Domino [2]int
 
-func Chainit(chain []Domino, pieces map[Wrapper]struct{}) ([]Domino, bool) {
+func Chainit(chain []Domino, pieces map[int]Domino) ([]Domino, bool) {
 	match := chain[len(chain)-1][1]
-	for k := range pieces {
-		piece := Domino{k.d[0], k.d[1]}
-		if piece[1] == match {
-			piece[0], piece[1] = piece[1], piece[0]
+	for key, domino := range pieces {
+		// create a new copy of the domino
+		d := Domino{domino[0], domino[1]}
+		if d[1] == match {
+			d[0], d[1] = d[1], d[0]
 		}
 
-		if piece[0] == match {
+		if d[0] == match {
 			// create a copy of chain and append the match
 			nextchain := []Domino{}
 			copy(chain, nextchain)
-			nextchain = append(chain, piece)
+			nextchain = append(chain, d)
 
 			// create a copy of pieces and remove the match
-			nextpieces := make(map[Wrapper]struct{})
-			for key, value := range pieces {
-				if key != k {
-					nextpieces[key] = value
+			nextpieces := make(map[int]Domino)
+			for k, v := range pieces {
+				if k != key {
+					nextpieces[k] = v
 				}
 			}
 			if len(nextpieces) > 0 {
 				if c, ok := Chainit(nextchain, nextpieces); ok {
 					return c, true
 				}
-			} else if nextchain[0][0] == piece[1] {
+			} else if nextchain[0][0] == d[1] {
 				return nextchain, true
 			}
 		}
@@ -35,31 +36,19 @@ func Chainit(chain []Domino, pieces map[Wrapper]struct{}) ([]Domino, bool) {
 	return chain, false
 }
 
-type Wrapper struct {
-	n int
-	d Domino
-}
-
 func MakeChain(input []Domino) ([]Domino, bool) {
-	chain := []Domino{}
-
-	// empty input is ok
 	if len(input) == 0 {
-		return chain, true
+		return input, true
 	}
-
 	start, match := input[0][0], input[0][1]
-	chain = append(chain, input[0])
-
-	// handle single domino
 	if len(input) == 1 {
-		return chain, start == match
+		return input, start == match
 	}
 
 	// put remaining dominos in a map for further processing
-	pieces := make(map[Wrapper]struct{}, len(input)-1)
+	pieces := make(map[int]Domino, len(input)-1)
 	for i, v := range input[1:] {
-		pieces[Wrapper{i, v}] = struct{}{}
+		pieces[i] = v
 	}
-	return Chainit(chain, pieces)
+	return Chainit([]Domino{input[0]}, pieces)
 }
